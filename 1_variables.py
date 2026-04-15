ac1f735d1a290e121a3a1ce7227c951c374f7272
gvilteedu-sys <gvilte.edu@gmail.com>
2026-04-14 21:59:47 -0300

    Teoria Nueva
    Asignacion y Variables

---
+++ 1_variables.py
@@ -0,0 +1,22 @@
+x="Esto es una variable"
+print(x)
+x= "ESTOS TAMBIEN ES UN VARIABLE"
+print(x)
+
+edadAlumno=12
+edad_Alumno=13#dsadsadsadsadsa
+_edadAlumno=23
+#2edad=12
+#edad-edadAlumno=12
+edad2=12
+#edad alumno=11
+texto="#@ 🏡​🏡​🏡​🏡​🏡​"
+print(texto)
+#dsadsadsadsadsa
+"""_summary_
+sdadsadsa
+dsadsa
+sdadsad
+dsadsadsa
+sadsadsa
+""" 
---
+++ 2_AsignacionMultiple.py
@@ -0,0 +1,7 @@
+x,y,z="Manzana","Naranja","Banana"
+print("\n"+x+"\n"+y+"\n"+z)
+
+a = b = c ="Mandarina"
+print(a,b,c)
+
+print("Mi fruta preferia es "+x)
